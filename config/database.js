aa92bf4f92587d7ebea9cb57b730d97bd073a617
olti0718 <oleg.titaev@netcracker.com>
2020-10-30 21:47:00 +0400

    aws integration

--- config/database.js
+++ config/database.js
@@ -4,11 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'terraform-20201027151000842300000001.c3lwgihmg0vi.eu-central-1.rds.amazonaws.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapidb'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'postgres'),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: false,
       },
     },
   },
